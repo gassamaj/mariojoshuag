31d5a9851b44fd30248620f6cfa9d20c85f972f1
Joshua Gassama <JoshuaGassama@yahoo.com>
2014-11-10 23:12:52 -0800

    Video 8

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -1,1 +1,17 @@
 // TODO
+game.PlayerEntity = me.Entity.extend({
+   init: function (x, y, settings) {
+        settings.image = "mario";
+        settings.spritewidth = "128";
+        settings.spriteheight = "128";
+        settings.height = 128;
+        settings.height  = 128;
+        this._super(me.Entity, 'init', [x, y, settings]);
+   
+   },
+
+  update: function() {
+
+  }
+
+});
