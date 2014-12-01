e0aea32efbaa819088ae1f9e50062bf45a436585
Joshua Gassama <JoshuaGassama@yahoo.com>
2014-12-01 13:52:12 -0800

    video 17

--- public_html/js/screens/title.js
+++ public_html/js/screens/title.js
@@ -2,16 +2,38 @@ game.TitleScreen = me.ScreenObject.extend({
 	/**	
 	 *  action to perform on state change
 	 */
-	onResetEvent: function() {	
-	   var titleImage = new me.Sprite(0, 0, me.loader.getImage("title-screen"));
-	   me.game.world.addChild(titleImage, 1);
+	onResetEvent: function() {
+       me.game.world.addChild( new me.Sprite (0, 0, me.loader.getImage('title-screen')), -10);
+       me.input.bindKey(me.input.KEY.ENTER, "start");
+       
+        me.game.world.addChild(new (me.Renderable.extend ({
+            init: function(){
+                this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+                this.font = new me.Font("turnup", 46, "white");    
+            
+            },
+            
+            draw: function(renderer){
+               this.font.draw(renderer.getContext(), "Marioish", 450, 130);
+               this.font.draw(renderer.getContext(), "Press Enter to play!", 250, 530);
+            }
+            
+            })));
+            
+            
+    this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
+         if(action === "start") {
+             me.state.change(me.state.PLAY);
+         }
+         });
         },
-	
-	
-	/**	
-	 *  action to perform when leaving this screen (state change)
-	 */
-	onDestroyEvent: function() {
-		; // TODO
-	}
-});
+       
+       
+       /**
+        *  action to perform when leaving this screen (state change)
+        */
+        onDestroyEvent: function () {
+        me.input.unbindKey(me.input.KEY.ENTER);
+        }   
+        });
+       
